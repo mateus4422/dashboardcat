5b9bbf45b6b3aa289a75d79c81f159fc0405ee53
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-27 15:33:49 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -24,37 +24,44 @@ dados_lojas_selecionadas = df[df["Loja"].isin(lojas_selecionadas)]
 # Organizar os blocos de total em uma grade
 total_container = st.container()
 total_container.markdown('<hr style="border:2px solid #FF6400">', unsafe_allow_html=True)
-total_block1, total_block2, total_block3, total_block4 = st.columns(4)
+total_block = st.columns(5)
 
 # Estilo para centralizar e formatar os valores
 value_style = "display: flex; justify-content: center; align-items: center; text-align: center; border: 2px solid #FF6400; padding: 10px; font-size: 20px;"
 
+# Função para formatar o valor em "R$ 75.809.091,57"
+def formatar_valor(valor):
+    return f"R$ {valor:,.2f}".replace(".", ",")
+
 # Bloco de Faturamento ST
-with total_block1:
-    st.subheader("Total Faturamento ST")
+with total_block[0]:
+    st.subheader("Faturamento ST")
     total_faturamento_st = dados_lojas_selecionadas["Faturamento ST"].sum()
-    st.markdown(f'<div style="{value_style}">R$ {total_faturamento_st:,.2f}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_faturamento_st)}</div>', unsafe_allow_html=True)
 
 # Bloco de Ressarcimento
-with total_block2:
-    st.subheader("Total Ressarcimento")
+with total_block[1]:
+    st.subheader("Ressarcimento")
     total_ressarcimento = dados_lojas_selecionadas["Ressarcimento"].sum()
-    st.markdown(f'<div style="{value_style}">R$ {total_ressarcimento:,.2f}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_ressarcimento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Complemento
-with total_block3:
-    st.subheader("Total Complemento")
+with total_block[2]:
+    st.subheader("Complemento")
     total_complemento = dados_lojas_selecionadas["Complemento"].sum()
-    st.markdown(f'<div style="{value_style}">R$ {total_complemento:,.2f}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_complemento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Diferença Ressarcimento - Complemento
-with total_block4:
+with total_block[3]:
     st.subheader("Diferença Ressarcimento - Complemento")
     diferenca_ressarcimento_complemento = total_ressarcimento - total_complemento
-    st.markdown(f'<div style="{value_style}">R$ {diferenca_ressarcimento_complemento:,.2f}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div style="{value_style}">{formatar_valor(diferenca_ressarcimento_complemento)}</div>', unsafe_allow_html=True)
 
-# Mostrar a média de ressarcimento
-media_percentual_ressarcimento = ((dados_lojas_selecionadas["Ressarcimento"] - 100) / dados_lojas_selecionadas["Faturamento ST"]).mean()
+# Bloco de Média % Ressarcimento
+with total_block[4]:
+    st.subheader("Média % Ressarcimento")
+    media_percentual_ressarcimento = dados_lojas_selecionadas["% Ressarcimento"].mean()
+    st.markdown(f'<div style="{value_style}">{media_percentual_ressarcimento:.2f}%</div>', unsafe_allow_html=True)
 
 # Espaço em branco entre os blocos
 st.markdown('<hr style="border:2px solid #FF6400">', unsafe_allow_html=True)
@@ -73,9 +80,4 @@ st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Complemento"], use_cont
 
 # Gráfico de Barras (Diferença Ressarcimento - Complemento)
 st.subheader("Gráfico de Barras (Diferença Ressarcimento - Complemento)")
-st.bar_chart(diferenca_ressarcimento_complemento, use_container_width=True)
-
-# Mostrar a média de ressarcimento no bloco de Diferença Ressarcimento - Complemento
-with total_block4:
-    st.subheader("Média % Ressarcimento")
-    st.markdown(f'<div style="{value_style}">{media_percentual_ressarcimento:.2%}</div>', unsafe_allow_html=True)
+st.bar_chart([diferenca_ressarcimento_complemento], use_container_width=True)

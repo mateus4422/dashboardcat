64be8f33f9afdd54ee55d3d84150416e77749be3
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-27 13:51:35 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -21,10 +21,10 @@ st.markdown(
 
 # Carregar os dados do Excel
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSulTerCVzXwOlraQucdzZsvxg-XGDZPA9xAXiMpFkQJ7GlfisoPoWzh3MrJEKCQPZYnDer7Cd0u5qE/pub?output=xlsx"
-df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8])  # Lê todas as linhas
+df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8, 9])  # Lê todas as colunas
 
 # Renomear as colunas
-df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "% Ressarcimento", "Status"]
+df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "Complemento", "% Ressarcimento", "Status"]
 
 # Filtro de Lojas
 lojas = df["Loja"].unique()
@@ -46,6 +46,16 @@ st.subheader("Total Ressarcimento")
 total_ressarcimento = dados_lojas_selecionadas["Ressarcimento"].sum()
 st.write(f"R$ {total_ressarcimento:,.2f}")
 
+# Bloco de Total Complemento
+st.subheader("Total Complemento")
+total_complemento = dados_lojas_selecionadas["Complemento"].sum()
+st.write(f"R$ {total_complemento:,.2f}")
+
+# Bloco de Ressarcimento - Complemento
+st.subheader("Ressarcimento - Complemento")
+diferenca_ressarcimento_complemento = total_ressarcimento - total_complemento
+st.write(f"R$ {diferenca_ressarcimento_complemento:,.2f}")
+
 # Bloco de Média % Ressarcimento
 st.subheader("Média % Ressarcimento")
 media_percentual_ressarcimento = (total_ressarcimento / total_faturamento_st) * 100
@@ -58,4 +68,3 @@ st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Faturamento ST"], use_c
 # Gráfico de Barras (Ressarcimento)
 st.subheader("Gráfico de Barras (Ressarcimento)")
 st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Ressarcimento"], use_container_width=True)
-

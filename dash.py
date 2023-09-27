627e648c2a3190bbd74a251e85d2f8631f4ccc2d
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-27 12:58:10 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Carregar os dados do Excel
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSulTerCVzXwOlraQucdzZsvxg-XGDZPA9xAXiMpFkQJ7GlfisoPoWzh3MrJEKCQPZYnDer7Cd0u5qE/pub?output=xlsx"
-df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8], skiprows=1)
+df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8], skiprows=0)  # Agora começa da primeira linha (skiprows=0)
 
 # Renomear as colunas
 df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "% Ressarcimento", "Status"]

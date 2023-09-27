cd12a416a767237af1e7983096af68f3fe096522
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-27 11:15:32 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-# Link para o arquivo do Google Sheets
+# Carregar os dados do Google Sheets
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSulTerCVzXwOlraQucdzZsvxg-XGDZPA9xAXiMpFkQJ7GlfisoPoWzh3MrJEKCQPZYnDer7Cd0u5qE/pubhtml"
-
-# Lê os dados do Google Sheets
 df = pd.read_html(url)[0]
 
-# Filtra os dados por Loja
+# Sidebar com filtro de lojas
+st.sidebar.header("Filtro de Lojas")
 lojas = df["Loja"].unique()
-filtro_loja = st.selectbox("Selecione uma Loja:", lojas)
-df_loja = df[df["Loja"] == filtro_loja]
+selected_lojas = st.sidebar.multiselect("Selecione as Lojas:", lojas)
 
-# Exibe os dados da Loja selecionada
-st.write(f"**Loja:** {filtro_loja}")
-st.write(f"**CNPJ:** {df_loja['CNPJ'].iloc[0]}")
-st.write(f"**Faturamento ST:** {df_loja['Faturamento ST'].iloc[0]}")
-st.write(f"**Ressarcimento:** {df_loja['Ressarcimento'].iloc[0]}")
-st.write(f"**% Ressarcimento:** {df_loja['% Ressarcimento'].iloc[0]}")
-st.write(f"**Status:** {df_loja['Status'].iloc[0]}")
+# Filtro de dados com base nas lojas selecionadas
+filtered_df = df[df["Loja"].isin(selected_lojas)]
+
+# Mostrar os dados filtrados
+st.write("Dados das Lojas Selecionadas:")
+st.write(filtered_df)
+
+# Resumo Geral
+st.header("Resumo Geral")
+st.write("Total de Faturamento ST:", filtered_df["Faturamento ST"].sum())
+st.write("Total de Ressarcimento:", filtered_df["Ressarcimento"].sum())
+st.write("% Ressarcimento Médio:", filtered_df["% Ressarcimento"].mean())
+st.write("Status Mais Comum:", filtered_df["Status"].mode().values[0])

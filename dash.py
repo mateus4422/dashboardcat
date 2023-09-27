a5d6e4779163aef4f17ac87766e136438f10c9b5
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-27 13:49:36 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -4,6 +4,21 @@ import pandas as pd
 # Exibir o logotipo
 st.image("farma.png", use_column_width=False, width=300)
 
+# Definir o CSS personalizado
+st.markdown(
+    """
+    <style>
+        .st-ff6400 {
+            background-color: #FF6400;
+        }
+        .st-ff6400:hover {
+            background-color: #FF6400;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 # Carregar os dados do Excel
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSulTerCVzXwOlraQucdzZsvxg-XGDZPA9xAXiMpFkQJ7GlfisoPoWzh3MrJEKCQPZYnDer7Cd0u5qE/pub?output=xlsx"
 df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8])  # Lê todas as linhas
@@ -13,7 +28,7 @@ df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento
 
 # Filtro de Lojas
 lojas = df["Loja"].unique()
-lojas_selecionadas = st.multiselect("Selecione as lojas:", lojas, default=lojas)
+lojas_selecionadas = st.multiselect("Selecione as lojas:", lojas, default=lojas, key="lojas", help="Escolha uma ou mais lojas")
 
 # Filtrar dados das lojas selecionadas
 dados_lojas_selecionadas = df[df["Loja"].isin(lojas_selecionadas)]
@@ -33,13 +48,14 @@ st.write(f"R$ {total_ressarcimento:,.2f}")
 
 # Bloco de Média % Ressarcimento
 st.subheader("Média % Ressarcimento")
-media_percentual_ressarcimento = dados_lojas_selecionadas["% Ressarcimento"].mean()
-st.write(f"{media_percentual_ressarcimento:.2%}")
+media_percentual_ressarcimento = (total_ressarcimento / total_faturamento_st) * 100
+st.write(f"{media_percentual_ressarcimento:.2f}%")
 
 # Gráfico de Barras (Faturamento ST)
 st.subheader("Gráfico de Barras (Faturamento ST)")
-st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Faturamento ST"])
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Faturamento ST"], use_container_width=True)
 
 # Gráfico de Barras (Ressarcimento)
 st.subheader("Gráfico de Barras (Ressarcimento)")
-st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Ressarcimento"])
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Ressarcimento"], use_container_width=True)
+

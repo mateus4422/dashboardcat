74c825a01399f61f5b6f5e1710c4c637ad87c9aa
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-29 15:33:17 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -9,30 +9,34 @@ url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSulTerCVzXwOlraQucdzZsvx
 df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8, 9])  # Lê todas as colunas
 
 # Renomear as colunas
-df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "Complemento", "% Ressarcimento", "Prioridade"]
+df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "Complemento", "% Ressarcimento", "Status"]
 
 # Função para formatar o valor em "R$ 75.809.091,57"
 def formatar_valor(valor):
     return f"R$ {valor:,.2f}".replace(".", ",")
 
-# Criar um menu lateral para seleção entre "Prioridade de Lojas" e "Geral"
-menu_selecionado = st.sidebar.radio("Selecione o Menu:", ["Prioridade de Lojas", "Geral"])
+# Widget de seleção de status
+status_selecionado = st.selectbox("Selecione o Status:", df["Status"].unique())
+
+# Filtrar dados pelo status selecionado
+dados_status_selecionado = df[df["Status"] == status_selecionado]
+
+# Adicionar menu lateral para escolher entre "Prioridade" e "Geral"
+menu_selecionado = st.sidebar.radio("Selecione o Menu:", ["Prioridade", "Geral"])
 
 # Filtro de Lojas
-lojas = df["Loja"].unique()
-lojas_selecionadas = st.sidebar.multiselect("Selecione as lojas:", lojas, default=lojas, key="lojas", help="Escolha uma ou mais lojas")
-
-# Filtro de Status
-status = df["Status"].unique()
-status_selecionado = st.sidebar.selectbox("Selecione o Status:", status)
-
-# Filtrar dados com base na seleção de lojas e status
-if menu_selecionado == "Prioridade de Lojas":
-    dados_filtrados = df[(df["Prioridade"] == "Sim") & (df["Loja"].isin(lojas_selecionadas)) & (df["Status"] == status_selecionado)]
+if menu_selecionado == "Prioridade":
+    lojas = dados_status_selecionado[dados_status_selecionado["Prioridade"] == "Sim"]["Loja"].unique()
 else:
-    dados_filtrados = df[(df["Prioridade"] != "Sim") & (df["Loja"].isin(lojas_selecionadas)) & (df["Status"] == status_selecionado)]
+    lojas = dados_status_selecionado["Loja"].unique()
 
-# Resto do código permanece o mesmo, usando os dados filtrados em "dados_filtrados"
+lojas_selecionadas = st.multiselect("Selecione as lojas:", ["Selecionar todos"] + lojas.tolist(), default="Selecionar todos", key="lojas", help="Escolha uma ou mais lojas")
+
+# Filtrar dados das lojas selecionadas
+if "Selecionar todos" not in lojas_selecionadas:
+    dados_lojas_selecionadas = dados_status_selecionado[dados_status_selecionado["Loja"].isin(lojas_selecionadas)]
+else:
+    dados_lojas_selecionadas = dados_status_selecionado
 
 # Organizar os blocos de total em uma grade
 total_container = st.container()
@@ -44,19 +48,19 @@ value_style = "display: flex; justify-content: center; align-items: center; text
 # Bloco de Faturamento ST
 with total_block[0]:
     st.subheader("Faturamento ST")
-    total_faturamento_st = dados_filtrados["Faturamento ST"].sum()
+    total_faturamento_st = dados_lojas_selecionadas["Faturamento ST"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_faturamento_st)}</div>', unsafe_allow_html=True)
 
 # Bloco de Ressarcimento
 with total_block[1]:
     st.subheader("Ressarcimento")
-    total_ressarcimento = dados_filtrados["Ressarcimento"].sum()
+    total_ressarcimento = dados_lojas_selecionadas["Ressarcimento"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_ressarcimento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Complemento
 with total_block[2]:
     st.subheader("Complemento")
-    total_complemento = dados_filtrados["Complemento"].sum()
+    total_complemento = dados_lojas_selecionadas["Complemento"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_complemento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Diferença Ressarcimento - Complemento
@@ -65,37 +69,36 @@ with total_block[3]:
     diferenca_ressarcimento_complemento = total_ressarcimento - total_complemento
     st.markdown(f'<div style="{value_style}">{formatar_valor(diferenca_ressarcimento_complemento)}</div>', unsafe_allow_html=True)
 
-# Bloco de Média % Ressarcimento
-with total_block[4]:
-    st.subheader("Média % Ressarcimento")
-
-    if menu_selecionado == "Prioridade de Lojas":
-        st.write("Apenas nos outros Status")
-    else:
-        # Certifique-se de que a variável dados_lojas_selecionadas foi definida
-        if "dados_lojas_selecionadas" in locals():
+# Calculadora de Média % Ressarcimento apenas para "Em Desenvolvimento"
+if status_selecionado == "Em Desenvolvimento":
+    with total_block[4]:
+        st.subheader("Média % Ressarcimento")
+        
+        if not dados_lojas_selecionadas.empty:
             media_percentual_ressarcimento = dados_lojas_selecionadas["% Ressarcimento"].mean() * 100
+            st.markdown(f'<div style="{value_style}">{media_percentual_ressarcimento:.1f}%</div>', unsafe_allow_html=True)
+
+            # Widget de entrada para a porcentagem
+            nova_porcentagem = st.number_input("Nova Porcentagem (%)", min_value=0.0, max_value=100.0, value=media_percentual_ressarcimento)
+            
+            # Calcular o novo valor de ressarcimento com base na nova porcentagem
+            novo_ressarcimento = total_faturamento_st * (nova_porcentagem / 100)
+            st.markdown(f'<div style="{value_style}">Novo Ressarcimento: {formatar_valor(novo_ressarcimento)}</div>', unsafe_allow_html=True)
         else:
-            media_percentual_ressarcimento = 0.0
-
-        st.markdown(f'<div style="{value_style}">{media_percentual_ressarcimento:.1f}%</div>', unsafe_allow_html=True)
-
-        # Widget de entrada para a porcentagem
-        nova_porcentagem = st.number_input("Nova Porcentagem (%)", min_value=0.0, max_value=100.0, value=media_percentual_ressarcimento / 100)
-
-# Calcular o novo valor de ressarcimento com base na nova porcentagem
-if menu_selecionado != "Prioridade de Lojas":
-    novo_ressarcimento = total_faturamento_st * nova_porcentagem
-    st.markdown(f'<div style="{value_style}">Novo Ressarcimento: {formatar_valor(novo_ressarcimento)}</div>', unsafe_allow_html=True)
+            st.write("Apenas nos Status 'Em Desenvolvimento'")
+else:
+    with total_block[4]:
+        st.subheader("Média % Ressarcimento")
+        st.write("Apenas nos outros Status")
 
 # Gráfico de Barras (Faturamento ST)
 st.subheader("Gráfico de Barras (Faturamento ST)")
-st.bar_chart(dados_filtrados.set_index("Loja")["Faturamento ST"], use_container_width=True)
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Faturamento ST"], use_container_width=True)
 
 # Gráfico de Barras (Ressarcimento)
 st.subheader("Gráfico de Barras (Ressarcimento)")
-st.bar_chart(dados_filtrados.set_index("Loja")["Ressarcimento"], use_container_width=True)
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Ressarcimento"], use_container_width=True)
 
 # Gráfico de Barras (Complemento)
 st.subheader("Gráfico de Barras (Complemento)")
-st.bar_chart(dados_filtrados.set_index("Loja")["Complemento"], use_container_width=True)
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Complemento"], use_container_width=True)

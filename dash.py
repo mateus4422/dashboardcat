593507bdaa238171a655ff3451423e798c8f8dc0
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-29 14:06:45 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 # Exibir o logotipo centralizado com tamanho 200x200
 st.image("farma.png", use_column_width=False, caption="", output_format="PNG", width=200)
@@ -12,61 +11,56 @@ df = pd.read_excel(url, usecols=[1, 2, 3, 4, 5, 6, 7, 8, 9])  # Lê todas as col
 # Renomear as colunas
 df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento ST", "Ressarcimento", "Complemento", "% Ressarcimento", "Status"]
 
-# Filtrar os status únicos disponíveis
-status = df["Status"].unique()
-selected_status = st.radio("Selecione o Status:", status)
+# Função para formatar o valor em "R$ 75.809.091,57"
+def formatar_valor(valor):
+    return f"R$ {valor:,.2f}".replace(".", ",")
 
-# Filtrar os dados com base no status selecionado
-filtered_data = df[df["Status"] == selected_status]
+# Widget de seleção de status
+status_selecionado = st.selectbox("Selecione o Status:", df["Status"].unique())
 
-# Verificar se há dados para o status selecionado
-if filtered_data.empty:
-    st.warning(f"Nenhum dado encontrado para o status '{selected_status}'.")
-else:
-    # Atualizar a lista de lojas disponíveis com base no status selecionado
-    lojas_disponiveis = filtered_data["Loja"].unique()
-    lojas_selecionadas = st.multiselect("Selecione as lojas:", lojas_disponiveis, key="lojas", help="Escolha uma ou mais lojas")
+# Filtrar dados pelo status selecionado
+dados_status_selecionado = df[df["Status"] == status_selecionado]
 
-    # Organizar os blocos de total em uma grade
-    total_container = st.container()
-    total_block = st.columns(5)
+# Filtro de Lojas
+lojas = dados_status_selecionado["Loja"].unique()
+lojas_selecionadas = st.multiselect("Selecione as lojas:", lojas, default=lojas, key="lojas", help="Escolha uma ou mais lojas")
 
-    # Estilo para centralizar e formatar os valores
-    value_style = "display: flex; justify-content: center; align-items: center; text-align: center; border: 2px solid #FF6400; padding: 10px; font-size: 20px;"
+# Filtrar dados das lojas selecionadas
+dados_lojas_selecionadas = dados_status_selecionado[dados_status_selecionado["Loja"].isin(lojas_selecionadas)]
 
-    # Função para formatar o valor em "R$ 75.809.091,57"
-    def formatar_valor(valor):
-        return f"R$ {valor:,.2f}".replace(".", ",")
+# Organizar os blocos de total em uma grade
+total_container = st.container()
+total_block = st.columns(5)
 
-    # Bloco de Faturamento ST
-    with total_block[0]:
-        st.subheader("Faturamento ST")
-        total_faturamento_st = filtered_data["Faturamento ST"].sum()
-        st.markdown(f'<div style="{value_style}">{formatar_valor(total_faturamento_st)}</div>', unsafe_allow_html=True)
+# Estilo para centralizar e formatar os valores
+value_style = "display: flex; justify-content: center; align-items: center; text-align: center; border: 2px solid #FF6400; padding: 10px; font-size: 20px;"
 
-    # Bloco de Ressarcimento
-    with total_block[1]:
-        st.subheader("Ressarcimento")
-        total_ressarcimento = filtered_data["Ressarcimento"].sum()
-        st.markdown(f'<div style="{value_style}">{formatar_valor(total_ressarcimento)}</div>', unsafe_allow_html=True)
+# Bloco de Faturamento ST
+with total_block[0]:
+    st.subheader("Faturamento ST")
+    total_faturamento_st = dados_lojas_selecionadas["Faturamento ST"].sum()
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_faturamento_st)}</div>', unsafe_allow_html=True)
 
-    # Bloco de Complemento
-    with total_block[2]:
-        st.subheader("Complemento")
-        total_complemento = filtered_data["Complemento"].sum()
-        st.markdown(f'<div style="{value_style}">{formatar_valor(total_complemento)}</div>', unsafe_allow_html=True)
+# Bloco de Ressarcimento
+with total_block[1]:
+    st.subheader("Ressarcimento")
+    total_ressarcimento = dados_lojas_selecionadas["Ressarcimento"].sum()
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_ressarcimento)}</div>', unsafe_allow_html=True)
 
-    # Bloco de Diferença Ressarcimento - Complemento
-    with total_block[3]:
-        st.subheader("Ressarcimento - Compl")
-        diferenca_ressarcimento_complemento = total_ressarcimento - total_complemento
-        st.markdown(f'<div style="{value_style}">{formatar_valor(diferenca_ressarcimento_complemento)}</div>', unsafe_allow_html=True)
+# Bloco de Complemento
+with total_block[2]:
+    st.subheader("Complemento")
+    total_complemento = dados_lojas_selecionadas["Complemento"].sum()
+    st.markdown(f'<div style="{value_style}">{formatar_valor(total_complemento)}</div>', unsafe_allow_html=True)
 
-    # Média % Ressarcimento geral (multiplicada por 100)
-    media_percentual_ressarcimento = filtered_data["% Ressarcimento"].mean() * 100
+# Bloco de Diferença Ressarcimento - Complemento
+with total_block[3]:
+    st.subheader("Ressarcimento - Compl")
+    diferenca_ressarcimento_complemento = total_ressarcimento - total_complemento
+    st.markdown(f'<div style="{value_style}">{formatar_valor(diferenca_ressarcimento_complemento)}</div>', unsafe_allow_html=True)
 
-  # Bloco de Média % Ressarcimento
-    with total_block[4]:
+# Bloco de Média % Ressarcimento
+with total_block[4]:
     st.subheader("Média % Ressarcimento")
 
     # Certifique-se de que a variável dados_lojas_selecionadas foi definida
@@ -84,16 +78,14 @@ else:
     novo_ressarcimento = total_faturamento_st * nova_porcentagem
     st.markdown(f'<div style="{value_style}">Novo Ressarcimento: {formatar_valor(novo_ressarcimento)}</div>', unsafe_allow_html=True)
 
+# Gráfico de Barras (Faturamento ST)
+st.subheader("Gráfico de Barras (Faturamento ST)")
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Faturamento ST"], use_container_width=True)
 
+# Gráfico de Barras (Ressarcimento)
+st.subheader("Gráfico de Barras (Ressarcimento)")
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Ressarcimento"], use_container_width=True)
 
-    # Gráfico de Barras (Faturamento ST)
-    st.subheader("Gráfico de Barras (Faturamento ST)")
-    st.bar_chart(filtered_data.set_index("Loja")["Faturamento ST"], use_container_width=True)
-
-    # Gráfico de Barras (Ressarcimento)
-    st.subheader("Gráfico de Barras (Ressarcimento)")
-    st.bar_chart(filtered_data.set_index("Loja")["Ressarcimento"], use_container_width=True)
-
-    # Gráfico de Barras (Complemento)
-    st.subheader("Gráfico de Barras (Complemento)")
-    st.bar_chart(filtered_data.set_index("Loja")["Complemento"], use_container_width=True)
+# Gráfico de Barras (Complemento)
+st.subheader("Gráfico de Barras (Complemento)")
+st.bar_chart(dados_lojas_selecionadas.set_index("Loja")["Complemento"], use_container_width=True)

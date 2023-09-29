b6e4c2e2511666a5dc76d6889a44b1ce0a2f07ff
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-09-29 15:31:45 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -15,16 +15,24 @@ df.columns = ["Período Inicial", "Período Final", "Loja", "CNPJ", "Faturamento
 def formatar_valor(valor):
     return f"R$ {valor:,.2f}".replace(".", ",")
 
-# Criar um menu suspenso para selecionar entre "Prioridade de Lojas" e "Geral"
-menu_selecionado = st.selectbox("Selecione o Menu:", ["Prioridade de Lojas", "Geral"])
+# Criar um menu lateral para seleção entre "Prioridade de Lojas" e "Geral"
+menu_selecionado = st.sidebar.radio("Selecione o Menu:", ["Prioridade de Lojas", "Geral"])
 
-# Filtrar dados com base na seleção do menu
+# Filtro de Lojas
+lojas = df["Loja"].unique()
+lojas_selecionadas = st.sidebar.multiselect("Selecione as lojas:", lojas, default=lojas, key="lojas", help="Escolha uma ou mais lojas")
+
+# Filtro de Status
+status = df["Status"].unique()
+status_selecionado = st.sidebar.selectbox("Selecione o Status:", status)
+
+# Filtrar dados com base na seleção de lojas e status
 if menu_selecionado == "Prioridade de Lojas":
-    dados_menu = df[df["Prioridade"] == "Sim"]
+    dados_filtrados = df[(df["Prioridade"] == "Sim") & (df["Loja"].isin(lojas_selecionadas)) & (df["Status"] == status_selecionado)]
 else:
-    dados_menu = df[df["Prioridade"] != "Sim"]
+    dados_filtrados = df[(df["Prioridade"] != "Sim") & (df["Loja"].isin(lojas_selecionadas)) & (df["Status"] == status_selecionado)]
 
-# Resto do código permanece o mesmo, usando os dados filtrados em "dados_menu"
+# Resto do código permanece o mesmo, usando os dados filtrados em "dados_filtrados"
 
 # Organizar os blocos de total em uma grade
 total_container = st.container()
@@ -36,19 +44,19 @@ value_style = "display: flex; justify-content: center; align-items: center; text
 # Bloco de Faturamento ST
 with total_block[0]:
     st.subheader("Faturamento ST")
-    total_faturamento_st = dados_menu["Faturamento ST"].sum()
+    total_faturamento_st = dados_filtrados["Faturamento ST"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_faturamento_st)}</div>', unsafe_allow_html=True)
 
 # Bloco de Ressarcimento
 with total_block[1]:
     st.subheader("Ressarcimento")
-    total_ressarcimento = dados_menu["Ressarcimento"].sum()
+    total_ressarcimento = dados_filtrados["Ressarcimento"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_ressarcimento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Complemento
 with total_block[2]:
     st.subheader("Complemento")
-    total_complemento = dados_menu["Complemento"].sum()
+    total_complemento = dados_filtrados["Complemento"].sum()
     st.markdown(f'<div style="{value_style}">{formatar_valor(total_complemento)}</div>', unsafe_allow_html=True)
 
 # Bloco de Diferença Ressarcimento - Complemento
@@ -64,9 +72,9 @@ with total_block[4]:
     if menu_selecionado == "Prioridade de Lojas":
         st.write("Apenas nos outros Status")
     else:
-        # Certifique-se de que a variável dados_menu foi definida
-        if "dados_menu" in locals():
-            media_percentual_ressarcimento = dados_menu["% Ressarcimento"].mean() * 100
+        # Certifique-se de que a variável dados_lojas_selecionadas foi definida
+        if "dados_lojas_selecionadas" in locals():
+            media_percentual_ressarcimento = dados_lojas_selecionadas["% Ressarcimento"].mean() * 100
         else:
             media_percentual_ressarcimento = 0.0
 
@@ -82,12 +90,12 @@ if menu_selecionado != "Prioridade de Lojas":
 
 # Gráfico de Barras (Faturamento ST)
 st.subheader("Gráfico de Barras (Faturamento ST)")
-st.bar_chart(dados_menu.set_index("Loja")["Faturamento ST"], use_container_width=True)
+st.bar_chart(dados_filtrados.set_index("Loja")["Faturamento ST"], use_container_width=True)
 
 # Gráfico de Barras (Ressarcimento)
 st.subheader("Gráfico de Barras (Ressarcimento)")
-st.bar_chart(dados_menu.set_index("Loja")["Ressarcimento"], use_container_width=True)
+st.bar_chart(dados_filtrados.set_index("Loja")["Ressarcimento"], use_container_width=True)
 
 # Gráfico de Barras (Complemento)
 st.subheader("Gráfico de Barras (Complemento)")
-st.bar_chart(dados_menu.set_index("Loja")["Complemento"], use_container_width=True)
+st.bar_chart(dados_filtrados.set_index("Loja")["Complemento"], use_container_width=True)
